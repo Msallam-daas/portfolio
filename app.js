ae73bc78249864599b2b1fd6a0ef1bba0f7ad09f
Msallam-daas <90200722+Msallam-daas@users.noreply.github.com>
2024-01-26 16:04:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function findMissingNumber(nums) {
+const n = nums.length;
+let expectedSum = n * (n + 1) / 2;
+let actualSum = nums.reduce((sum, num) => sum + num, 0);
+return expectedSum - actualSum;
+}
+
+
+
+
